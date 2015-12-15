15c080372c179e32ae4f0dcdda2f4868dbc69fe7
Olaf Lessenich <lessenic@fim.uni-passau.de>
2015-12-15 16:15:23 +0100

    Add expected results for ChangedMethod.java
    Signed-off-by: Olaf Lessenich <lessenic@fim.uni-passau.de>

---
+++ threeway/combined/SimpleTests/ChangedMethod.java
@@ -0,0 +1,9 @@
+public class SameStatement {
+<<<<<<< testfiles/threeway/left/SimpleTests/ChangedMethod.java
+=======
+public void n() {
+    int a = 11;
+  }
+>>>>>>> testfiles/threeway/right/SimpleTests/ChangedMethod.java
+
+}
