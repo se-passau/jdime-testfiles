0959a955cf7dcba3066d32e0429778cb7eb4fdcf
Olaf Lessenich <lessenic@fim.uni-passau.de>
2015-12-15 16:06:49 +0100

    Add expected results for VariableDeclaration scenario.
    Signed-off-by: Olaf Lessenich <lessenic@fim.uni-passau.de>

---
+++ threeway/structured/SimpleTests/VariableDeclaration.java
@@ -0,0 +1,10 @@
+public class SameStatement {
+  public void n() {
+    int a = 
+<<<<<<< testfiles/threeway/left/SimpleTests/VariableDeclaration.java
+11
+=======
+>>>>>>> testfiles/threeway/right/SimpleTests/VariableDeclaration.java
+;
+  }
+}
