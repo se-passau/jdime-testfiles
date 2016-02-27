292e5dae384c2dd446981cfb7ef99660e260431f
Olaf Lessenich <lessenic@fim.uni-passau.de>
2016-02-28 00:04:02 +0100

    Add more expected results for semistructured merge
    Signed-off-by: Olaf Lessenich <lessenic@fim.uni-passau.de>

---
+++ threeway/semistructured/SimpleTests/ChangedMethod2.java
@@ -0,0 +1,9 @@
+public class SameStatement {
+<<<<<<< Unknown file: This is a bug in JDime.
+=======
+public void n() {
+    int a;
+  }
+>>>>>>> /home/lessenic/git/jdime/../jdime-testfiles/threeway/right/SimpleTests/ChangedMethod2.java
+
+}
