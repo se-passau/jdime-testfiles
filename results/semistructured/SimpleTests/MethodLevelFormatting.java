4047155f46f3f01ce92dc08495f366127a104baa
Georg Seibt <seibt@fim.uni-passau.de>
2017-04-04 12:58:26 +0200

    Correct semistructured merge result, the conflict in the method body is expected.

--- results/semistructured/SimpleTests/MethodLevelFormatting.java
+++ results/semistructured/SimpleTests/MethodLevelFormatting.java
@@ -4,14 +4,19 @@ import java.io.OutputStream;
 
 public class MethodLevelFormatting {
   public void method() {
-    if (Math.random() > .6f) {
-      System.out.println("40% chance!");
-    }
-    try (
-      OutputStream out = new FileOutputStream("out");) {
-      out.write(21);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+
+        if (Math.random() > .6f) {
+            System.out.println("40% chance!");
+        }
+
+<<<<<<<
+        try (OutputStream out = new FileOutputStream("out")) { out.write(21); } catch (IOException e) {
+=======
+        try (OutputStream out = new FileOutputStream("out")) {
+            out.write(42);
+        } catch (IOException e) {
+>>>>>>>
+            e.printStackTrace();
+        }
   }
 }
